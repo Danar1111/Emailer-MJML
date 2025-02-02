e5c3b01d5b86c6b822ed039e50afd671322d738c
Danar <priyambodo02@gmail.com>
2025-02-02 20:34:20 +0700

    update dynamic mjml template

--- server.js
+++ server.js
@@ -19,16 +19,42 @@ const transporter = nodemailer.createTransport({
     }
 });
 
+function fillTemplate(template, data) {
+    return template
+        .replace(/{{user_name}}/g, data.user_name)
+        .replace(/{{image}}/g, data.image)
+        .replace(/{{generated}}/g, data.generated)
+        .replace(/{{generated_by}}/g, data.generated_by)
+        .replace(/{{regards}}/g, data.regards)
+        .replace(/{{company}}/g, data.company)
+        .replace(/{{current_year}}/g, data.current_year)
+        .replace(/{{link}}/g, data.link);
+}
+
 async function sendEmail(toEmail) {
     const templatePath = path.join(__dirname, 'emailBody/email.mjml');
     const htmlContent = fs.readFileSync(templatePath, 'utf8');
-    const { html } = mjml(htmlContent);
+
+    const emailData = {
+        user_name: 'User Name',
+        image: 'https://picsum.photos/400',
+        generated: 'The future belongs to those who believe in the beauty of their dreams.',
+        generated_by: 'Eleanor Roosevelt',
+        regards: 'Owner Name',
+        company: 'Company Name',
+        current_year: new Date().getFullYear(),
+        link: "link_example"
+    };
+
+    const renderedTemplate = fillTemplate(htmlContent, emailData);
+
+    const { html } = mjml(renderedTemplate);
 
     try {
         const info = await transporter.sendMail({
             from: process.env.EMAIL_FROM,
             to: toEmail,
-            subject: 'Hi!',
+            subject: 'Hi There!',
             html: html,
         });
 
