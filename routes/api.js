8a99e7b04c003cafa7128381fecb03ba5e110cff
Danar <priyambodo02@gmail.com>
2025-02-12 15:38:12 +0700

    create front end and endpoint to fill the form

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,27 @@
 import express from 'express';
 import { upload, uploadFile } from '../controllers/userController.js';
+import path, { join } from 'path';
+import { fileURLToPath } from 'url';
+import { send } from 'process';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const router = express.Router();
+
+router.get(
+    '/',
+    (req, res) => {
+        res.sendFile(path.join(__dirname, "..", "public", "message.html"));
+    }
+);
+
+router.get(
+    '/success',
+    (req, res) => {
+        res.sendFile(path.join(__dirname, "..", "public", "success.html"));
+    }
+);
 
 router.post(
     '/upload',
